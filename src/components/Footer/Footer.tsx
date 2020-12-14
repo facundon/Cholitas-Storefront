64166672720bbf745c6de888c4c35cb27ce33340
Facundo <facundonrodriguez@gmail.com>
2020-12-14 14:28:11 -0300

    minor visual fixes added mercadopago safty script

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,11 +9,15 @@ import Nav from "./Nav";
 const Footer: React.FC = () => (
   <div className="footer" id="footer">
     <div className="footer__favicons container">
-      {SOCIAL_MEDIA.map(medium => (
+      {/* {SOCIAL_MEDIA.map(medium => (
         <SocialMediaIcon medium={medium} key={medium.ariaLabel} />
-      ))}
+      ))} */}
     </div>
     <Nav />
+    <div className="footer__mp container">
+      <img src="https://imgmp.mlstatic.com/org-img/banners/ar/medios/online/785X40.jpg" 
+        title="Mercado Pago - Medios de pago" alt="Mercado Pago - Medios de pago" />
+    </div>
   </div>
 );
 
